d21817ca3b1f32f1e7c67f536f0712e8c3748996
souparnodas244-cmd <souparnodas244@gmail.com>
2025-12-15 00:04:49 +0530

    added python logic

--- logic.py
+++ logic.py
@@ -1,23 +1,13 @@
+
 a,b,c=100,100,100
-dict={500:a,200:b,100:c}
+
+l2=[]
 target=int(input())
-
-def expand(target):
-    l=[]
-    i=0
-    while target>0:
-        b=target%10
-        c=b*(10**i)
-        if c!=0 :
-            l.append(c)
-        target=target//10
-        i+=1
-    return l
-
-def despensing(x): 
+def money_dispence(target): 
+    temp=target 
     for i in dict:
-        count=0
-        if x%i==0:
-            count+=1
-        
-
+        count=temp//i
+        temp=temp-(count*i)
+        l2.append(count)
+    
+    
